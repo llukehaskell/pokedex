f158010c29ea72ffa5d3509c0fe39774f8a8720d
llukehaskell <lukehaskell@gmail.com>
2023-05-22 14:02:45 -0400

    reworked api call functionality

--- src/app/listofpokemon/listofpokemon.component.ts
+++ src/app/listofpokemon/listofpokemon.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonComponent } from '../pokemon/pokemon.component';
 import { PokeapiService } from '../pokeapi.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-listofpokemon',
@@ -8,13 +9,25 @@ import { PokeapiService } from '../pokeapi.service';
   styleUrls: ['./listofpokemon.component.css']
 })
 export class ListofpokemonComponent implements OnInit{
+  data: any = {};// "= {}" gets rid of the console undefined errors
   public pokelist: PokemonComponent[] = [];
 
-  constructor(pokeapi: PokeapiService) {
-    this.pokelist = pokeapi.getPokemons(1, 9); //inclusive
-    console.log(this.pokelist);
-  }
+  constructor(private pokeapi: PokeapiService) { }
   
-  ngOnInit(): void { 
+  ngOnInit(): void {
+    this.pokeapi.getPokemons(1).pipe(tap((res) => {
+      this.data = res;
+      let tmpmon: PokemonComponent = new PokemonComponent; // new PokeComp is needed here for class vars & methods to exist
+      
+      //all of these don't seem to care they're supposed to be a certain type, theyll just be anything if u want. weird
+      tmpmon.setIdx(+(this.data.id));
+      tmpmon.setName(String(this.data.name));
+      tmpmon.setTypeA(this.data.types[0].type.name); 
+      if (Object.keys(this.data.types).length == 2) {
+        tmpmon.setTypeB(this.data.types[1].type.name);
+      }
+      
+      this.pokelist.push(tmpmon);
+    })).subscribe();
   }
 }
--- src/app/pokeapi.service.ts
+++ src/app/pokeapi.service.ts
@@ -1,38 +1,14 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, timeout } from 'rxjs';
-import { PokemonComponent } from './pokemon/pokemon.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeapiService {
 
-  private pokelist: PokemonComponent[] = [];
-  constructor(private http: HttpClient) { } //idk why http is special and needs to be initialized here but ok
-    
-  getPokemons(idxStart: number, idxEnd: number) {
-    for (let i = idxStart; i < (idxEnd + 1); ++i) {
-      this.callAPI(i);
-    }
-    return this.pokelist;
-  }
+  constructor(private http: HttpClient) { }
   
-  callAPI(x: number) {
-    this.http.get<JSON>('https://pokeapi.co/api/v2/pokemon/' + x).subscribe((res) => {
-      let data: any = res;
-      let tmpmon: PokemonComponent = new PokemonComponent; // new PokeComp is needed here for class vars & methods to exist
-      
-      //all of these don't seem to care they're supposed to be a certain type, theyll just be anything if u want. weird
-      tmpmon.setIdx(+(data.id));
-      tmpmon.setName(String(data.name));
-      tmpmon.setTypeA(data.types[0].type.name); 
-      if (Object.keys(data.types).length == 2) {
-        tmpmon.setTypeB(data.types[1].type.name);
-      }
-      
-      this.pokelist.push(tmpmon);
-      // console.log(this.pokelist);
-    });
-  }
+  getPokemons(x: number) {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/' + x);
+  };
 }
