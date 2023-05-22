3f2a8e9bc4934f96f5122b1d0dcfd226cf107f23
llukehaskell <lukehaskell@gmail.com>
2023-05-22 16:53:20 -0400

    hardcoded ugly & sometimes unsorted MVP achieved

--- src/app/listofpokemon/listofpokemon.component.ts
+++ src/app/listofpokemon/listofpokemon.component.ts
@@ -10,24 +10,30 @@ import { tap } from 'rxjs';
 })
 export class ListofpokemonComponent implements OnInit{
   data: any = {};// "= {}" gets rid of the console undefined errors
-  public pokelist: PokemonComponent[] = [];
+  public pokelist: any = [];
 
   constructor(private pokeapi: PokeapiService) { }
   
   ngOnInit(): void {
-    this.pokeapi.getPokemons(1).pipe(tap((res) => {
-      this.data = res;
-      let tmpmon: PokemonComponent = new PokemonComponent; // new PokeComp is needed here for class vars & methods to exist
-      
-      //all of these don't seem to care they're supposed to be a certain type, theyll just be anything if u want. weird
-      tmpmon.setIdx(+(this.data.id));
-      tmpmon.setName(String(this.data.name));
-      tmpmon.setTypeA(this.data.types[0].type.name); 
-      if (Object.keys(this.data.types).length == 2) {
-        tmpmon.setTypeB(this.data.types[1].type.name);
-      }
-      
-      this.pokelist.push(tmpmon);
-    })).subscribe();
+    for (let i = 1; i < 10; ++i) {
+      this.pokeapi.getPokemons(i).pipe(tap((res) => {
+        this.data = res;
+        let tmpmon: PokemonComponent = new PokemonComponent; // new PokeComp is needed here for class vars & methods to exist
+        
+        //all of these don't seem to care they're supposed to be a certain type, theyll just be anything if u want. weird
+        tmpmon.setIdx(+(this.data.id));
+        tmpmon.setName(String(this.data.name));
+        tmpmon.setNumTypes(Object.keys(this.data.types).length);
+        tmpmon.setTypeA(this.data.types[0].type.name); 
+        if (tmpmon.getNumTypes() == 2) {
+          tmpmon.setTypeB(this.data.types[1].type.name);
+        }
+        
+        //https://medium.com/compendium/5-helpful-rxjs-solutions-d34f7c2f1cd9
+
+        this.pokelist.push(tmpmon);
+        console.log(this.pokelist);
+      })).subscribe();
+    }
   }
 }
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -2,27 +2,26 @@ import { Component, Inject } from '@angular/core';
 
 //i think this is prolly useless, might have to make it a class but at that point idrc i can just store the types as strings
 export enum poketype {
-  "none" = 0,
-  "normal" = 1,
-  "fire" = 2,
-  "water" = 3,
-  "grass" = 4,
-  "electric" = 5,
-  "ice" = 6,
-  "fighting" = 7,
-  "poison" = 8,
-  "ground" = 9,
-  "flying" = 10,
-  "psychic" = 11,
-  "bug" = 12,
-  "rock" = 13,
-  "ghost" = 14,
-  "dragon" = 15,
-  "dark" = 16,
-  "steel" = 17,
-  "fairy" = 18
-} //idk if export is bad here or not but idk how other files are gonna know
-  //what a poketype is
+  NONE = 0,
+  NORMAL = 1,
+  FIRE = 2,
+  WATER = 3,
+  GRASS = 4,
+  ELECTRIC = 5,
+  ICE = 6,
+  FIGHTING = 7,
+  POISON = 8,
+  GROUND = 9,
+  FLYING = 10,
+  PSYCHIC = 11,
+  BUG = 12,
+  ROCK = 13,
+  GHOST = 14,
+  DRAGON = 15,
+  DARK = 16,
+  STEEL = 17,
+  FAIRY = 18
+} 
 
 @Component({
   selector: 'app-pokemon',
@@ -32,20 +31,23 @@ export enum poketype {
 export class PokemonComponent {
   constructor(
     @Inject(String) private name: string = "mantiquatro", 
-    @Inject(Number) private idx: number = 9999,  
-    @Inject(poketype) private typea = poketype.none,
-    @Inject(poketype) private typeb = poketype.none
+    @Inject(Number) private idx: number = 9999,
+    @Inject(Number) private numTypes = 2,
+    @Inject(poketype) private typeA = poketype.NONE,
+    @Inject(poketype) private typeB = poketype.NONE
   ) { } //note @Inject syntax for types
 
   //setters
   public setName(name: string) {this.name = name;}
   public setIdx(idx: number) {this.idx = idx;}
-  public setTypeA(typea: poketype) {this.typea = typea;}
-  public setTypeB(typeb: poketype) {this.typeb = typeb;}
+  public setNumTypes(numTypes: number) {this.numTypes = numTypes;}
+  public setTypeA(typea: poketype) {this.typeA = typea;}
+  public setTypeB(typeb: poketype) {this.typeB = typeb;}
 
   //getters
   public getName() {return this.name;}
   public getIdx() {return this.idx;}
-  public getTypeA() {return this.typea;}
-  public getTypeB() {return this.typeb;}
+  public getNumTypes() {return this.numTypes;}
+  public getTypeA() {return this.typeA;}
+  public getTypeB() {return this.typeB;}
 }
